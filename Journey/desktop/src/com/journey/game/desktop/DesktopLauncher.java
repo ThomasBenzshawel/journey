e4d4188dee8d3f79a0e12d9c8476c61808fc3473
Thomas Benzshawel <93297588+ThomasBenzshawel@users.noreply.github.com>
2022-01-27 22:57:06 -0600

    New FILES!

---
+++ Journey/desktop/src/com/journey/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.journey.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.journey.game.Journey;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Journey(), config);
+	}
+}
